b7ddad1afb247c4eabf4dce800b2bde79b8087e6
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2024-08-26 09:51:09 +0300

    ..

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -4,10 +4,16 @@ import PostDetailes from "../../components/postDetails";
 export default async function page({ params }) {
   // from url
   const postId = params.postId;
-    const loadingJSX = (<div><h1>Loading</h1></div>);
+  const loadingJSX = (
+    <div>
+      <h1>Loading</h1>
+    </div>
+  );
   //   console.log("===================", params.postId);
   return (
     <div style={{ padding: "20xp" }}>
+
+      
       <h1>Post Detailes</h1>
 
       <Suspense fallback={loadingJSX}>
